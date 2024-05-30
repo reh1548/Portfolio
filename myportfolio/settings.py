2247c23f40cc1cf82cee53c8aa4dc1c02b68d68e
Rehanul Ahmed <ra3238159@gmail.com>
2024-05-31 00:04:23 +0600

    set for vercel deployment

--- myportfolio/settings.py
+++ myportfolio/settings.py
@@ -40,7 +40,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '.vercel.app']
 
 
 # Application definition
